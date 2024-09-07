ab9d391e92aa3123de62cb5a605debc085353018
scarlhs26 <lycrek@gmail.com>
2024-09-07 12:03:20 -0400

    cambios github page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/PruebaTecnica-Tacklist/', // Cambia 'PruebaTecnica-Tacklist' por el nombre de tu repositorio
+  plugins: [react()]
+});
